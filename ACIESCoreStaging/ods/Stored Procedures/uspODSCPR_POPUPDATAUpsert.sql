9c4788b794dbaf34069a9d97a67a40d9e4de26bc
Mukund Dugam <MDugam@paragonhealthcare.com>
2022-05-19 04:29:21 -0500

    POPUPDATA and INSCOMP

---
+++ ACIESCoreStaging/ods/Stored Procedures/uspODSCPR_POPUPDATAUpsert.sql
@@ -0,0 +1,72 @@
+﻿CREATE PROCEDURE [ods].[uspODSCPR_POPUPDATAUpsert]
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+    DECLARE @DateTime DATETIME = GETDATE();
+
+    -- Update existing records to previous version
+    UPDATE [ods].[CPR_POPUPDATA]
+    SET [CurrentRowFlag] = 0,
+        [ExpirationDate] = @DateTime,
+        [UpdateDate] = @DateTime
+    FROM [ods].[CPR_POPUPDATA] [t]
+        INNER JOIN [stage].[CPR_POPUPDATA] [s]
+            ON [t].[CPK_POPUPDATA] = [s].[CPK_POPUPDATA]
+               AND [t].[CurrentRowFlag] = 1;
+
+    SET @DateTime = DATEADD(SS, 1, @DateTime);
+
+    -- Insert New and modified records as current version
+    INSERT INTO [ods].[CPR_POPUPDATA]
+    ([CPK_POPUPDATA]
+      ,[TEXT_]
+      ,[CODE]
+      ,[CFK_POPUPS]
+      ,[POPUPNAME]
+      ,[DELFLAG]
+      ,[CREATEDON]
+      ,[TOUCHEDBY]
+      ,[TOUCHDATE]
+      ,[CREATEDBY]
+      ,[CHGBYHOST]
+      ,[AMOUNT]
+      ,[CFK_TABLE]
+      ,[ITEMDATE]
+      ,[EffectiveDate]
+      ,[ExpirationDate]
+      ,[CurrentRowFlag]
+      ,[DeleteRowFlag]
+      ,[InsertDate]
+    )
+    SELECT [CPK_POPUPDATA]
+      ,[TEXT_]
+      ,[CODE]
+      ,[CFK_POPUPS]
+      ,[POPUPNAME]
+      ,[DELFLAG]
+      ,[CREATEDON]
+      ,[TOUCHEDBY]
+      ,[TOUCHDATE]
+      ,[CREATEDBY]
+      ,[CHGBYHOST]
+      ,[AMOUNT]
+      ,[CFK_TABLE]
+      ,[ITEMDATE]
+      , @DateTime AS [EffectiveDate]
+      , NULL AS [ExpirationDate]
+      , 1 AS [CurrentRowFlag]
+      , 0 AS [DeleteRowFlag]
+      , @DateTime AS [InsertDate]
+    FROM [stage].[CPR_POPUPDATA] AS [s]
+    WHERE NOT EXISTS
+    (
+        SELECT *
+        FROM [ods].[CPR_POPUPDATA] AS [t]
+        WHERE [t].[CPK_POPUPDATA] = [s].[CPK_POPUPDATA]
+              AND [t].[CurrentRowFlag] = 1
+    );
+
+END;
+
---
+++ ACIESCoreStaging/ods/Tables/CPR_POPUPDATA.sql
@@ -0,0 +1,39 @@
+﻿
+CREATE TABLE [ods].[CPR_POPUPDATA](
+	[RecordID] [INT] IDENTITY(1, 1) NOT NULL,
+	[CPK_POPUPDATA] [int] NOT NULL,
+	[TEXT_] [varchar](125) NULL,
+	[CODE] [varchar](100) NULL,
+	[CFK_POPUPS] [int] NULL,
+	[POPUPNAME] [varchar](20) NULL,
+	[DELFLAG] [int] NOT NULL,
+	[CREATEDON] [datetime] NULL,
+	[TOUCHEDBY] [varchar](200) NULL,
+	[TOUCHDATE] [datetime] NULL,
+	[CREATEDBY] [varchar](100) NULL,
+	[CHGBYHOST] [varchar](100) NULL,
+	[AMOUNT] [numeric](15, 4) NULL,
+	[CFK_TABLE] [int] NULL,
+	[ITEMDATE] [datetime] NULL,
+	[EffectiveDate] [DATETIME] NULL,
+    [ExpirationDate] [DATETIME] NULL,    
+    [CurrentRowFlag] [BIT] NOT NULL,
+    [DeleteRowFlag] [BIT] NOT NULL,
+    [InsertDate] [DATETIME] NULL,
+    [UpdateDate] [DATETIME] NULL,
+    CONSTRAINT [PKods.CPR_POPUPDATA.RecordID]
+        PRIMARY KEY CLUSTERED ([RecordID] ASC)
+) ON [PRIMARY]
+GO
+ALTER TABLE [ods].[CPR_POPUPDATA]
+ADD CONSTRAINT [ods.CPR_POPUPDATAEffectiveDate]
+    DEFAULT (GETDATE()) FOR [EffectiveDate]
+GO
+ALTER TABLE [ods].[CPR_POPUPDATA]
+ADD CONSTRAINT [ods.CPR_POPUPDATADeleteRowFlag]
+    DEFAULT ((0)) FOR [DeleteRowFlag]
+GO
+ALTER TABLE [ods].[CPR_POPUPDATA]
+ADD CONSTRAINT [ods.CPR_POPUPDATAInsertDate]
+    DEFAULT (GETDATE()) FOR [InsertDate]
+GO
